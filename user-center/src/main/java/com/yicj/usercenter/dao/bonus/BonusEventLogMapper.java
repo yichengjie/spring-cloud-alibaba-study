1d932b49c03865396bdd17440109cfc78e05238f
易成杰 <626659321@qq.com>
2020-10-06 20:55:07 +0800

    保存

---
+++ user-center/src/main/java/com/yicj/usercenter/dao/bonus/BonusEventLogMapper.java
@@ -0,0 +1,7 @@
+package com.yicj.usercenter.dao.bonus;
+
+import com.yicj.usercenter.domain.entity.bonus.BonusEventLog;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BonusEventLogMapper extends Mapper<BonusEventLog> {
+}
---
+++ user-center/src/main/java/com/yicj/usercenter/rocketmq/AddBonusListener.java
@@ -0,0 +1,41 @@
+package com.yicj.usercenter.rocketmq;
+
+import com.yicj.usercenter.dao.bonus.BonusEventLogMapper;
+import com.yicj.usercenter.dao.user.UserMapper;
+import com.yicj.usercenter.domain.dto.message.UserAddBonusMsgDTO;
+import com.yicj.usercenter.domain.entity.bonus.BonusEventLog;
+import com.yicj.usercenter.domain.entity.user.User;
+import lombok.RequiredArgsConstructor;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+@RocketMQMessageListener(consumerGroup = "consumer-group",topic = "add-bonus")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class AddBonusListener implements RocketMQListener<UserAddBonusMsgDTO> {
+    private final UserMapper userMapper ;
+    private final BonusEventLogMapper bonusEventLogMapper ;
+
+    @Override
+    public void onMessage(UserAddBonusMsgDTO message) {
+        //当收到消息时执行的业务
+        //1. 为用户添加积分
+        Integer userId = message.getUserId();
+        Integer bonus = message.getBonus();
+        User user = userMapper.selectByPrimaryKey(userId);
+        user.setBonus(user.getBonus() + bonus);
+        userMapper.updateByPrimaryKey(user) ;
+        //2. 记录日志到bonus_event_log表
+        BonusEventLog bonusEventLog = BonusEventLog.builder()
+                .userId(userId)
+                .value(bonus)
+                .event("CONTRIBUTE")
+                .createTime(new Date())
+                .description("投稿加积分")
+                .build();
+        bonusEventLogMapper.insert(bonusEventLog) ;
+    }
+}
